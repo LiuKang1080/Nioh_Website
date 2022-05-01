4fac0a123558ce85bd6d0a7a710f51e68c4adb7a
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-05-01 19:04:04 -0400

    Adjusted how far the screen needs to scroll before the back-to-top-button appears in test.js

--- Nioh2/Testing/test.js
+++ Nioh2/Testing/test.js
@@ -23,7 +23,7 @@ back_to_top = document.getElementById("back-to-top-button");
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
         back_to_top.style.display = "block";
     } else {
         back_to_top.style.display = "none";
