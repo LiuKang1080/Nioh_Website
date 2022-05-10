9749f2d54e6e3dc9c84c3417c856cb4adb20d983
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-05-10 19:55:45 -0400

    Updated all of the pages for Dream of the Samurai, Strong, Demon, Wise, Nioh, and Twilight Missions.

---
+++ Nioh2/missions/Demon/js/demon.js
@@ -0,0 +1,26 @@
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function region1() {
+    let element = document.getElementById("region1");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
