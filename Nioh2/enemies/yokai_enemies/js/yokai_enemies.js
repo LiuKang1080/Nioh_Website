5a413ea8b6a233b9e0d5c2612415e290ad9122d1
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-05-11 20:18:51 -0400

    Updated the grid for general.css. Finished enemies/human_enemies.html. Started work on enemies/yokai_enemies.html

---
+++ Nioh2/enemies/yokai_enemies/js/yokai_enemies.js
@@ -0,0 +1,45 @@
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function base() {
+    let element = document.getElementById("base");
+    element.scrollIntoView();
+}
+
+function dlc1() {
+    let element = document.getElementById("dlc1");
+    element.scrollIntoView();
+}
+
+function dlc2() {
+    let element = document.getElementById("dcl2");
+    element.scrollIntoView();
+}
+
+function dlc3() {
+    let element = document.getElementById("dlc3");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
