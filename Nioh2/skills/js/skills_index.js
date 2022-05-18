333ba9f9a9a2461da50376dd6a3800a2ad01e1af
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-05-17 20:21:00 -0400

    Updated general.css. Added unicodes.txt. Completed skills_index.html. Added skills_index.css and .js.

---
+++ Nioh2/skills/js/skills_index.js
@@ -0,0 +1,35 @@
+/* JavaScript for skills_index.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function stat_scaling() {
+    let element = document.getElementById("stat-scaling");
+    element.scrollIntoView();
+}
+
+function skill_trees() {
+    let element = document.getElementById("skill-trees");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
