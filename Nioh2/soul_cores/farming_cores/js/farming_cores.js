2042d168e3647c2cb5cf9594910bc1f36f2a7861
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-05-20 21:01:00 -0400

    Started work on farming_cores.html. Added farming_cores.js

---
+++ Nioh2/soul_cores/farming_cores/js/farming_cores.js
@@ -0,0 +1,30 @@
+/* JavaScript for farming_cores.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
