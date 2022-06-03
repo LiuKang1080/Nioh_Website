ed24541b791b7aad907ff83f7d2a91ae7816c646
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-06-03 19:45:15 -0400

    Added melee.html and .js. Updated general.css

---
+++ Nioh2/blacksmith/Tables/js/melee.js
@@ -0,0 +1,40 @@
+/* JavaScript for melee.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function standard() {
+    let element = document.getElementById("standard");
+    element.scrollIntoView();
+}
+
+function nioh() {
+    let element = document.getElementById("nioh");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
