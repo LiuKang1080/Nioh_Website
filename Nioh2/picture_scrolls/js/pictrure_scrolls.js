ee76553d5bc155fcb24127bfb4504e25cbdc19cb
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-06-14 22:29:22 -0400

    Added more content to picture_scrolls.html. Added picture_scrolls.js.

---
+++ Nioh2/picture_scrolls/js/pictrure_scrolls.js
@@ -0,0 +1,45 @@
+/* JavaScript for picture_scrolls.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function attribute() {
+    let element = document.getElementById("attribute");
+    element.scrollIntoView();
+}
+
+function ultimate() {
+    let element = document.getElementById("ultimate");
+    element.scrollIntoView();
+}
+
+function path() {
+    let element = document.getElementById("path");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
