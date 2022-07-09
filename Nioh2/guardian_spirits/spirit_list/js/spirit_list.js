bb1c4e709b4073d2ad0bfc2a7fa6363b7dc92647
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-07-08 23:16:53 -0400

    Added more content to spirit_list.html. Added spirit_list.css and .js. Started to test image layouts in core_list.html. Added respective images in /guardian_spirit/img and /soul_cores/img.

---
+++ Nioh2/guardian_spirits/spirit_list/js/spirit_list.js
@@ -0,0 +1,19 @@
+// JavaScript for spirit_list.html
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
