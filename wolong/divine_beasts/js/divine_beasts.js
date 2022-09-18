a1e0f3b07881b2841f37ae00ba43bccb15b98b6b
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-09-18 00:33:41 -0400

    Updated Nioh 2's credit.html. Added credits.html, divine_beasts.html .css and .js. Added the respective images in divine_beasts/img/. Updated wolong_index.html with the proper href link.

---
+++ wolong/divine_beasts/js/divine_beasts.js
@@ -0,0 +1,35 @@
+// JavaScript for divine_beasts.html
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function beast_list() {
+    let element = document.getElementById("beast-list");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
